23fe9cc175511e39f3914390bd8d743a58782cb2
SupriyaKuncha <kunchasupriya181@gmail.com>
2024-08-10 17:10:13 +0530

    installed and configured ubuntu terminalon windows

--- cutExample.sh
+++ cutExample.sh
@@ -1,1 +1,11 @@
-echo hello
+#!/bin/bash 
+
+#command to run bash script in windows
+#install gitbash
+#open terminal in VS code and run the command [ bash filename.sh ]
+
+# while read n
+# do 
+#     echo ${n} | cut -c 3
+# done 
+
